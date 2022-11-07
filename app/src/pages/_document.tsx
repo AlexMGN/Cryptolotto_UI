1b088df1fa0c58c0df39d75368c3961fe019d37c
Mignon Alexandre <alex.mignon77@gmail.com>
2022-11-07 17:39:03 +0100

    Update Phantom image

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -14,7 +14,9 @@ class MyDocument extends Document {
           <title>Cryptolotto</title>
           <meta property="og:title" content="Cryptolotto" />
           <meta property="og:type" content="gambling.game.solana" />
-          <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
+          <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/local-assets.appspot.com/o/Cryptolotto_logo_mini.png?alt=media&token=77853071-42de-4190-899f-e9b67ace7072" />
+          <meta property="og:description" content="" />
+          <meta name="description" content="" />
           <link rel="Cryptolotto shortcut icon" href="/Cryptolotto_logo_mini.png"/>
           <link rel="apple-touch-icon" href="/Cryptolotto_logo_mini.png"/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet" />
